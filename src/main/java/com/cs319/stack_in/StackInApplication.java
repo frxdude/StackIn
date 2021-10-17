fcb94af721c855e4c5c25659515b74bded38685a
frxdude <goodaberg103@gmail.com>
2021-10-17 23:13:44 +0800

    init

---
+++ src/main/java/com/cs319/stack_in/StackInApplication.java
@@ -0,0 +1,13 @@
+package com.cs319.stack_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs319/stack_in/StackInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs319.stack_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
