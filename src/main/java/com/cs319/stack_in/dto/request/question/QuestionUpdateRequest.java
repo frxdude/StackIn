03658dad0a6e092e7289250966dc1eb523e0773f
ariunaa <g.ariunaa11@gmail.com>
2021-11-23 22:02:17 +0800

    Question apis updated

--- src/main/java/com/cs319/stack_in/dto/request/question/QuestionUpdateRequest.java
+++ src/main/java/com/cs319/stack_in/dto/request/question/QuestionUpdateRequest.java
@@ -1,36 +1,7 @@
-package com.cs319.stack_in.dto.request.question;
-
-import lombok.*;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-/**
- * QuestionUpdataRequest
+package com.cs319.stack_in.dto.request.question;/**
+ * QuestionUpdateRequest
  *
  * @author Ariunaa Gantumur
  **/
-
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class QuestionUpdateRequest {
-    @NotBlank(message = "{val.not.null}")
-    private String title;
-
-    @NotBlank(message = "{val.not.null}")
-    private String description;
-
-    private Long correctAnswerId;
-
-    @NotNull(message = "{val.not.null}")
-    private Long id;
-
-    @NotNull(message = "{val.not.null}")
-    private Long userId;
-
-    @NotNull(message = "{val.not.null}")
-    private int upVotes;
 }
