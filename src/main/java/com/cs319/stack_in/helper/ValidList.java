4d0c532ae0e3432ff614ee4e7ae4a9df326d08ed
frxdude <goodaberg103@gmail.com>
2021-11-23 21:12:01 +0800

    valid list added

---
+++ src/main/java/com/cs319/stack_in/helper/ValidList.java
@@ -0,0 +1,22 @@
+package com.cs319.stack_in.helper;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Delegate;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ValidList
+ *
+ * @author Sainjargal Ishdorj
+ **/
+@Data
+@ToString
+public class ValidList<E> implements List<E> {
+    @Valid
+    @Delegate
+    private List<E> list = new ArrayList<>();
+}
