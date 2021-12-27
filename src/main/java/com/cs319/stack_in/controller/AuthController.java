7ddfbc740f2d8aa56324a4fb1497a6817c844fbb
frxdude <goodaberg103@gmail.com>
2021-12-28 02:00:33 +0800

    cross origin allowed for all

--- src/main/java/com/cs319/stack_in/controller/AuthController.java
+++ src/main/java/com/cs319/stack_in/controller/AuthController.java
@@ -26,6 +26,7 @@ import java.io.UnsupportedEncodingException;
 @Api(tags = "Auth")
 @RestController
 @RequestMapping("auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController {
 
     AuthService service;
